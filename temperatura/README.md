996c721e0a237ad945adb9287744cc4b290bef46
Repastero <alt.f3-3e1gsud@speed.1s.fr>
2021-10-06 10:44:42 -0300

    Mejoras en script de temperatura media 'get_smn_mean_temp.py'
    Eliminada opción para agregar manualmente valores de pronóstico.
    Agregada opción para obtener automáticamente la temp. pronóstico de 14 días.
    Agregada provincia La Rioja y sus estaciones meteorológicas.
    Ahora se genera un archivo de salida por cada provincia indicada.

--- temperatura/README.md
+++ temperatura/README.md
@@ -1,19 +1,50 @@
 ## Generar archivo con valores de temperatura media diarios
 - ***SMN*** informa hasta 365 días previos a la fecha de ejecución (no inclusive).
 - Modificar valores en script:
-  - **WEATHER_STATIONS**: lista de centrales meteorológicas en orden alfabético.
+  - **PROVINCES_IDX**: *(opcional)* comentar / eliminar las provincias no deseadas.
+  - **STATIONS_NAME**: *(opcional)* modificar centrales meteorológicas de cada provincia, en orden sur a norte.
     * Fuente: https://ssl.smn.gob.ar/dpd/zipopendata.php?dato=estaciones
-  - **REGIONS_STATIONS_IDX**: índices en orden sur-centro-norte de centrales.
-  - **OUTPUT_FILE**: nombre archivo de salida.
-  - **FORECAST**: suma de temperatura pronostico mínima y máxima de centrales sur a norte, o nulo para omitir.
+  - **FORECAST_ENABLED**: obtiene los valores de temperatura media de los próximos 14 días.
   - **START_DATE**: día del primer valor (en formato DDMMYYYY).
   - **END_DATE**: día del ultimo valor (en formato DDMMYYYY), o nulo para infinito.
+- Para poder obtener pronostico, instalar estas librerías individualmente o desde requirement:
+```
+pip install -U requests
+pip install -U lxml
+o
+pip install -r requirements.txt
+```
 - Correr script desde consola.
   - Si todo funciona correctamente, la salida debe ser similar a:
 ```
-END_DATE 28072021
-START_DATE 01012021
-Archivo de salida: 2021-smn-pna.csv
+Archivo SMN: registro_temperatura365d_smn.txt
+Fecha fin: 05/10/2021
+Fecha inicio: 01/01/2021
+
+Leyendo pronostico de: GUALEGUAYCHU AERO
+Ultima actualizacion: 21:42:56 05/10/2021
+Leyendo pronostico de: PARANA AERO
+Ultima actualizacion: 21:13:10 05/10/2021
+Leyendo pronostico de: CONCORDIA AERO
+Ultima actualizacion: 22:07:10 05/10/2021
+> Archivo de salida: 2021-entreri.csv
+
+Leyendo pronostico de: ROSARIO AERO
+Ultima actualizacion: 22:04:42 05/10/2021
+Leyendo pronostico de: SAUCE VIEJO AERO
+Ultima actualizacion: 22:07:14 05/10/2021
+Leyendo pronostico de: RECONQUISTA AERO
+Ultima actualizacion: 22:07:16 05/10/2021
+> Archivo de salida: 2021-santafe.csv
+
+Leyendo pronostico de: CHEPES
+Ultima actualizacion: 22:07:19 05/10/2021
+Leyendo pronostico de: CHAMICAL AERO
+Ultima actualizacion: 22:07:22 05/10/2021
+Leyendo pronostico de: LA RIOJA AERO
+Ultima actualizacion: 21:43:13 05/10/2021
+> Archivo de salida: 2021-larioja.csv
+
 ```
 - Fuente de URLs de descarga:
   - https://www.smn.gob.ar/descarga-de-datos
--- temperatura/get_smn_mean_temp.py
+++ temperatura/get_smn_mean_temp.py
@@ -3,102 +3,199 @@
 import os
 import urllib.request
 import zipfile
+from time import sleep
+from datetime import datetime, timedelta
 
-WEATHER_STATIONS = ['CONCORDIA AERO', 'GUALEGUAYCHU AERO', 'PARANA AERO'] # orden alfabetico - ER
-#WEATHER_STATIONS = ['RECONQUISTA AERO', 'ROSARIO AERO', 'SAUCE VIEJO AERO'] # orden alfabetico - SF
-REGIONS_STATIONS_IDX = [1,2,0] # indices en orden sur a norte
-INPUT_FILE_URL = 'https://ssl.smn.gob.ar/dpd/zipopendata.php?dato=regtemp'
-OUTPUT_FILE = '2021-smn-pna.csv'
+INPUT_FILE_URL  = 'https://ssl.smn.gob.ar/dpd/zipopendata.php?dato=regtemp'
 
-# Valores de pronostico de sur a norte (sumar minima y maxima) o None para ovbiar
-FORECAST = [
-[24,27,33,30,32,26,15, 12,19,25,21,22,25,26],
-[24,28,35,32,32,28,18, 13,18,24,21,26,26,25],
-[23,31,37,38,36,32,20, 14,18,26,23,27,27,28]]
+FORECAST_ENABLED    = True
+FORECAST_URLS       = ['https://www.tiempo.com/{0}.htm', 'https://www.tiempo.com/argentina/{0}/proxima-semana'] # 7 + 7 dias de pronostico
 
-# < a END_DATE
-START_DATE  = '01012021'
-# > a START_DATE o None para tomar dsd ultima fecha
-END_DATE    = None
+START_DATE  = '01012021' # < a END_DATE
+END_DATE    = None # > a START_DATE o None para tomar desde ultima fecha
+
+# Indice de vectores de estaciones met. por provincias
+PROVINCES_IDX = { # comentar / eliminar las no deseadas
+    'entreri' : 0,
+    'santafe' : 1,
+    'larioja' : 2,
+}
+# Estaciones en orden de sur a norte
+STATIONS_NAME = [
+    ['GUALEGUAYCHU AERO', 'PARANA AERO', 'CONCORDIA AERO'],
+    ['ROSARIO AERO', 'SAUCE VIEJO AERO', 'RECONQUISTA AERO'],
+    ['CHEPES', 'CHAMICAL AERO', 'LA RIOJA AERO'], # o CHILECITO AERO
+]
+# Parametro para 'FORECAST_URLS', mismo orden que STATIONS_NAME
+STATIONS_FORECAST_URL = [
+    ['gualeguaychu', 'parana', 'concordia'],
+    ['rosario', 'sauce-viejo', 'reconquista'],
+    ['chepes', 'chamical', 'la-rioja_argentina-l16904'], # o chilecito
+]
 
 def read_smn_file():
     smn_file_lines = None
     try:
         filehandle, _ = urllib.request.urlretrieve(INPUT_FILE_URL)
-        #filehandle = 'Registro_temperaturas-19052021.zip' # uso manual
+        #filehandle = 'Registro_temperaturas-06102021.zip' # uso manual
         zip_file_object = zipfile.ZipFile(filehandle, 'r')
-        first_file = zip_file_object.namelist()[0]
-        smn_file = zip_file_object.open(first_file)
-        smn_file_lines = smn_file.readlines()
+        file_info = zip_file_object.infolist()[0]
+        print(f'Archivo SMN: {file_info.filename}')
+        smn_file = zip_file_object.open(file_info.filename, 'r')
+        # Leo todo el archivo en memoria por que solo contiene info. de 365 dias
+        smn_file_lines = smn_file.read().decode('utf-8', 'ignore').splitlines()
         smn_file.close()
         zip_file_object.close()
         os.remove(zip_file_object.filename)
     except Exception as e:
-        print('Error al descargar archivo:', e)
+        print('Error al descargar archivo smn', e, sep='\n')
     return smn_file_lines
 
-def get_avg_temp(val):
+def get_avg_temp(max, min):
+    _ = (float(max) + float(min)) / 2
+    _ += 0.001
+    return round(_, 1)
+
+def get_avg_temp_joined(val):
     temps = val.split()
     if len(temps) > 1:
-        _ = (float(temps[0]) + float(temps[1])) / 2
-        _ += 0.001
-        return round(_, 1)
+        return get_avg_temp(temps[0], temps[1])
     else:
         return 404
 
+def get_date(value, date_format='%Y-%m-%d%Z%I:%M:%S%z'):
+    new_date = None
+    try:
+        new_date = datetime.strptime(value, date_format)
+    except:
+        pass
+    return new_date
+
+def get_forecast_values(req_session, station):
+    temps_forecast = []
+    try:
+        for idx, url in enumerate(FORECAST_URLS):
+            resp = req_session.get(url.format(station))
+            tree = html.fromstring(resp.content)
+            # La primer consulta se fija si la fecha pronosticada es la siguiente a la ultima registrada
+            if idx == 0:
+                date_list = tree.xpath('//meta[@property="og:updated_time"]/@content') # fecha actualizacion pronostico
+                if date_list and END_DATE:
+                    forecast_date = get_date(date_list[0]) # formato ISO
+                    forecast_date = forecast_date.replace(tzinfo=None) - timedelta(hours=3) # paso a UTC-3
+                    print('Ultima actualizacion:', forecast_date.strftime('%H:%M:%S %d/%m/%Y'))
+                    last_date = get_date(END_DATE, '%d%m%Y') # formato smn
+                    # Si hay mas de un dia de diferencia, se saltea el pronostico
+                    _ = (forecast_date - last_date).days
+                    if _ > 1:
+                        print(f'Error: Pronostico {_} dias adelantado, ignorado')
+                        break
+            # Lee maximas y minimas pronosticadas, y calcula el promedio
+            max_temps = tree.xpath('//span[@class="datos-dos-semanas"]//span[@class="maxima changeUnitT"]/@data')
+            min_temps = tree.xpath('//span[@class="datos-dos-semanas"]//span[@class="minima changeUnitT"]/@data')
+            _ = [get_avg_temp(max_temps[i].split('|')[0], min_temps[i].split('|')[0]) for i in range(len(max_temps))]
+            temps_forecast.extend(_)
+            sleep(0.5) # para demorar la proxima consulta
+    except Exception as e:
+        print('Error al intentar obtener pronostico:', e, sep='\n')
+    return temps_forecast
+
+# Verifica que existan las librerias para obtener pronostico
+if FORECAST_ENABLED:
+    try:
+        from requests import Session
+        from lxml import html, etree
+    except ModuleNotFoundError as e:
+        print(e)
+        FORECAST_ENABLED = False
+
+# Carga en una lista las lineas del archivo TXT de SMN
 temp_lines = read_smn_file()
 if not temp_lines:
     exit('No se obtuvo archivo con temperaturas')
 
-temperatures = []
+# Listas con temperaturas por estaciones
+output_dict = dict()
+for prov_idx in PROVINCES_IDX.values():
+    for st in STATIONS_NAME[prov_idx]:
+        output_dict[st] = []
+
+# Lee TXT linea por linea y guarda los valores de estaciones conocidas
+station_name_col = -1
 found_start = False
 found_end   = False
 for line in temp_lines:
-    line = line.decode('utf8', 'ignore') # hace falta pq viene en bytes
-    for i in range(len(WEATHER_STATIONS)):
-        idx = line.find(WEATHER_STATIONS[i])
-        if idx != -1:
+    if station_name_col == -1:
+        station_name_col = line.find('NOMBRE') # nombre estacion
+    else:
+        station_name = line[station_name_col:].rstrip()
+        # Busca lista con temperaturas promedio de estacion
+        temp_values = output_dict.get(station_name)
+        if temp_values != None:
+            temp_date = line[:8] # Fecha en formato %d%m%Y
+            # Chequea si primer dia
             if not found_start:
-                if not END_DATE or line[:8] == END_DATE:
-                    print("END_DATE", line[:8])
+                if not END_DATE or temp_date == END_DATE:
+                    _ = get_date(temp_date, '%d%m%Y')
+                    print('Fecha fin:', _.strftime('%d/%m/%Y'))
+                    END_DATE = temp_date
                     found_start = True
                 else:
+                    # Sigue leyendo hasta encontrar fecha inicio
+                    continue
+            # Chequea si ultimo dia
+            if not found_end:
+                if temp_date == START_DATE:
+                    _ = get_date(temp_date, '%d%m%Y')
+                    print('Fecha inicio:', _.strftime('%d/%m/%Y'))
+                    found_end = True
+            else:
+                # Si ya paso la fecha fin, deja de leer
+                if temp_date != START_DATE:
                     break
-            # Primera central
-            if i == 0:
-                temperatures.append([0]*len(WEATHER_STATIONS))
-            # Extrae TMAX TMIN
-            _ = get_avg_temp(line[9:idx-1])
+            # Extrae TMAX TMIN y guarda promedio
+            _ = get_avg_temp_joined(line[9:station_name_col-1])
             if _ != 404:
-                temperatures[-1][i] = _
-            else: # FALTA DATO!!!
-                #print(line[:8], WEATHER_STATIONS[i])
-                temperatures[-1][i] = temperatures[-2][i]
-            # Chequea si primer valor de ultima central
-            if line[:8] == START_DATE and i == len(WEATHER_STATIONS)-1:
-                print("START_DATE", line[:8])
-                found_end = True
-    if found_end:
-        break
+                temp_values.append(_)
+            else: # Falta dato
+                if not temp_values: # lista vacia, invento valor
+                    temp_values.append(12.5)
+                else: # copio anterior
+                    temp_values.append(temp_values[-1])
 
-if not temperatures:
-    exit('No se obtuvieron valores de temperaturas')
-
-# Invertir y agregar pronostico
-temperatures.reverse() # vienen al revez
-if FORECAST:
-    for i in range(len(FORECAST[0])):
-        _ = [0]*len(WEATHER_STATIONS)
-        for j,idx in enumerate(REGIONS_STATIONS_IDX):
-            _[idx] = FORECAST[j][i] / 2
-        temperatures.append(_)
-
-# Salida
-out_file = open(OUTPUT_FILE, 'w', encoding='utf-8')
-out_file.write('temp sur;temp centro;temp norte\n')
-for temp in temperatures:
-    _ = ';'.join([str(temp[idx]).rjust(4," ") for idx in REGIONS_STATIONS_IDX])
-    out_file.write(f'{_}\n')
-out_file.flush()
-out_file.close()
-print(f'Archivo de salida: {out_file.name}')
+# Por cada provincia, guarda el rchivo de salida con datos de sus 3 estaciones
+request_session = None # para pronostico
+print() # blank line
+for prov_key, prov_idx in PROVINCES_IDX.items():
+    temperatures = []
+    min_days = 999
+    for st_index, st_name in enumerate(STATIONS_NAME[prov_idx]):
+        temps = output_dict.get(st_name)
+        # Invertir y agregar pronostico
+        temps.reverse() # vienen al revez
+        if FORECAST_ENABLED:
+            print('Leyendo pronostico de:', st_name)
+            if not request_session:
+                request_session = Session()
+            _ = get_forecast_values(request_session, STATIONS_FORECAST_URL[prov_idx][st_index])
+            temps.extend(_)
+        # Guardo la cantidad minim de dias, para verificar
+        if len(temps) < min_days:
+            min_days = len(temps)
+        # Guarda copia a lista de salida
+        temperatures.append(temps)
+    
+    # Verificar que tenga muestras de las 3 estaciones
+    if min_days == 0 or len(temperatures) < 3:
+        print('No se obtuvieron todos los valores de temperaturas de:', prov_key)
+        continue
+    
+    # Escrbir salida
+    out_file = open(f'{END_DATE[-4:]}-{prov_key}.csv', 'w', encoding='utf-8')
+    out_file.write('temp sur;temp centro;temp norte\n')
+    for i in range(min_days):
+        _ = ';'.join([str(temps[i]).rjust(4," ") for temps in temperatures])
+        out_file.write(f'{_}\n')
+    out_file.flush()
+    out_file.close()
+    print(f'> Archivo de salida: {out_file.name}', end='\n\n')
